0d3d97cebbbade8f2008650373b9881cce2a33a7
javatech147 <javatech147@gmail.com>
2019-12-17 01:04:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/crashlyticsdemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.crashlyticsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
